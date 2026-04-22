be706f877a2dcb9cd15f39a892111c0a034694dc
YunisSadig <yunissadiq6@gmail.com>
2026-04-22 17:02:45 +0400

    small improvement

--- src/main/java/com/example/lmsproject/assignmentSubmission/AssignmentSubmissionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.lmsproject.assignmentSubmission;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AssignmentSubmissionRepository extends JpaRepository<AssignmentSubmission,Long> {
-}
